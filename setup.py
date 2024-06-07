0ba5ba03a69e0926bcb1c08ffe87f4b018b0ace6
Supriyo chakraborty <supriyochakraborty954@gmail.com>
2024-06-07 12:04:57 +0530

    logging and exception

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ def get_requirements(file_path:str)->List[str]:
 setup(
 name='mlproject',
 version='0.0.1',
-author='supriyo',
+author='Supriyo',
 author_email='supriyochakraborty954@gmail.com',
 packages=find_packages(),
 install_requires=get_requirements('requirements.txt')
